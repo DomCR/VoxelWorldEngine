b3c10425b2739193e08368a9aa36114bfd632f64
Dom <SnowDC93@users.noreply.github.com>
2019-10-22 17:12:31 +0200

    First update, empty project
    Starting tests with world generator

---
+++ Assets/VoxelWorldEngine/Chunk.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace VoxelWorldEngine
+{
+    public class Chunk : MonoBehaviour
+    {
+        public const int XSize = 16;
+        public const int YSize = 256;
+        public const int ZSize = 16;
+    }
+}
---
+++ Assets/VoxelWorldEngine/WorldGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VoxelWorldEngine
+{
+    public class WorldGenerator : MonoBehaviour
+    {
+        public GameObject ChunkPrefab;
+
+        [Space()]
+        public int ChunksX;
+        public int ChunksY;
+        public int ChunksZ;
+
+        public readonly int k = 4;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            GenerateWorld();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        //****************************************************************
+        void GenerateWorld()
+        {
+            for (int x = 0; x < ChunksX; x++)
+            {
+                for (int y = 0; y < ChunksY; y++)
+                {
+                    for (int z = 0; z < ChunksZ; z++)
+                    {
+                        GameObject tmp_chunk = Instantiate(ChunkPrefab, new Vector3(x * Chunk.XSize, y * Chunk.YSize, z * Chunk.ZSize), new Quaternion(0,0,0,0), this.transform);
+                    }
+                }
+            }
+        }
+    }
+}
